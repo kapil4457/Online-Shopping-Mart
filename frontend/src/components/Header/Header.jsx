280708168f261b1b50606769462aca945b2fd2fd
Kapil Soni <kapilsoni54768161@gmail.com>
2022-10-13 13:45:16 +0530

    Temporarily done with the header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Header.css";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import GavelOutlinedIcon from "@mui/icons-material/GavelOutlined";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+
+const Header = () => {
+  return (
+    <div className="main-header">
+      <div className="first-part">
+        <NavLink to="/" className="left">
+          <div>
+            <LocalMallIcon />
+            <span>Kharido</span>
+          </div>
+          <p>Jee Bhar K</p>
+        </NavLink>
+
+        <div className="right">
+          <div className="main-bar">
+            <div className="search-bar">
+              <input type="text" placeholder="Search Items" />
+              <SearchOutlinedIcon />
+            </div>
+          </div>
+
+          <NavLink to="/my-orders" className="my-orders">
+            <GavelOutlinedIcon />
+            <p>My Orders</p>
+          </NavLink>
+          <NavLink to="/cart" className="cart">
+            <ShoppingCartOutlinedIcon />
+            <p>Cart</p>
+          </NavLink>
+          <NavLink to="/account" className="accountSection">
+            <img src="logo.png" alt="" />
+          </NavLink>
+        </div>
+      </div>
+      <div className="second-part">
+        <div className="all"></div>
+        <div className="other-options"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

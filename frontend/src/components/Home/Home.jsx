7ee52eb95bbc9b166d97afa1a531df31269a40a0
Kapil Soni <kapilsoni54768161@gmail.com>
2022-10-16 09:38:58 +0530

    Updated the Home by adding two more layers of offer

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -74,7 +74,7 @@ const Home = () => {
               <img src="ac.jpg" alt="AC " />
               <p>Inverter AC | Upto 60% off</p>
             </NavLink>
-            <NavLink to="Micro" className="micro">
+            <NavLink to="/products/Micro" className="micro">
               <img src="micro.jpg" alt="MIcroWave" />
               <p>MicroWaves | Starting 9,640</p>
             </NavLink>
@@ -109,10 +109,64 @@ const Home = () => {
         </div>
       </div>
       <div className="block-buster">
+        <h2>BlockBuster Deals</h2>
         <Slider data={BlockBuster} />
       </div>
-      <div>Under 399 kitchen</div>
-      <div>Deo</div>
+      <div className="kitchen-items">
+        <h2>Kitchen Items Under 399</h2>
+        <Slider data={kitchen} />
+      </div>
+
+      <div className="main-body-home">
+        <div className="womens-store">
+          <h2 className="heading">Starting 199 | Women's Fashion</h2>
+          <div className="womens-clothing">
+            <NavLink to="/products/Womens-Clothing" className="W-clothing">
+              <img src="w1.jpg" alt="kurti" />
+              <p>Womens Clothing</p>
+            </NavLink>
+            <NavLink to="/products/Women-BackPack" className="WBackpack">
+              <img src="w2.jpg" alt="W-BackPack" />
+              <p>BackPack</p>
+            </NavLink>
+            <NavLink to="/products/Women-Beauty" className="WBeauty">
+              <img src="w3.jpg" alt="W-Beauty " />
+              <p>Beauty</p>
+            </NavLink>
+            <NavLink to="products/Women-Jewellery" className="WJewellery">
+              <img src="w4.jpg" alt="W-Jewellery" />
+              <p>Fashion Jewellery</p>
+            </NavLink>
+          </div>
+        </div>
+        <div className="deals-on-beauty">
+          <h2 className="heading">Deals on beauty | Upto 50% off</h2>
+          <div className="womens-clothing">
+            <NavLink to="/products/Skincare" className="skincare">
+              <img src="b1.jpg" alt="skincare" />
+              <p>Deals on SkinCare</p>
+            </NavLink>
+            <NavLink to="/products/Haircare" className="haircare">
+              <img src="b2.jpg" alt="Haircare" />
+              <p>Haircare</p>
+            </NavLink>
+            <NavLink to="/products/Makeup" className="makeup">
+              <img src="b3.jpg" alt="MakeUp" />
+              <p>MakeUp</p>
+            </NavLink>
+            <NavLink to="products/BathAndShower" className="BathAndShower">
+              <img src="b4.jpg" alt="BathAndShower" />
+              <p>Bath And Shower</p>
+            </NavLink>
+          </div>
+        </div>
+        <div className="smart-tv">
+          <h2>Up to 60% off | Upgrade your Laptops</h2>
+          <NavLink to="/products/Laptops">
+            <img src="laptop.jpg" alt="Laptop" />
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 };

aa45ab72464e8183a37ec36538e63a872175d4ef
Kapil Soni <kapilsoni54768161@gmail.com>
2022-10-20 23:20:05 +0530

    Added the No items found page

--- frontend/src/components/SearchResult/SearchPageResult.jsx
+++ frontend/src/components/SearchResult/SearchPageResult.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import SeearchCardResult from "./SearchCardResult.jsx";
 
 const SearchPageResult = ({ data, range, brand }) => {
   let params = useParams();
-
+  const navigate = useNavigate();
   const filtering = (val) => {
     if (val.price >= range.minPrice && val.price <= range.maxPrice) {
       if (brand == "") {
@@ -22,11 +22,24 @@ const SearchPageResult = ({ data, range, brand }) => {
         <h2>Search Result for '{params.name}'</h2>
         <h3>Item Count : {data.length}</h3>
       </div>
-      <div className="searchResultCard">
-        {data.filter(filtering).map((item, key) => (
-          <SeearchCardResult data={item} key={key} />
-        ))}
-      </div>
+      {data.length == 0 ? (
+        <div className="no-items">
+          <p>No Items</p>
+          <button
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Continue Shopping
+          </button>
+        </div>
+      ) : (
+        <div className="searchResultCard">
+          {data.filter(filtering).map((item, key) => (
+            <SeearchCardResult data={item} key={key} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };

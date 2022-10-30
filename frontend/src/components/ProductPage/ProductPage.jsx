b9de4f926225f64ce5042f910c77c244647e03eb
Kapil Soni <kapilsoni54768161@gmail.com>
2022-10-31 00:10:14 +0530

    Created the update product detail card on the admin side

--- frontend/src/components/ProductPage/ProductPage.jsx
+++ frontend/src/components/ProductPage/ProductPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductPage.css";
 import Sidebar from "../SideBar/Sidebar";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,6 +10,10 @@ import { deleteProduct } from "../../redux/actions/productAction";
 
 const ProductPage = () => {
   const { loading, products } = useSelector((state) => state.getAllProducts);
+  const [display, setDisplay] = useState("none");
+  // console.log(products?.products);
+
+  const [data, setData] = useState({});
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -68,16 +72,108 @@ const ProductPage = () => {
                         onClick={() => {
                           dispatch(deleteProduct(item._id));
                           window.location.reload();
-                          //   dispatch(getAllProducts());
                         }}
                       >
                         Delete
                       </button>
-                      <button className="update">Update</button>
+                      <button
+                        className="update"
+                        onClick={() => {
+                          setDisplay("flex");
+                          setData(item);
+                        }}
+                      >
+                        Update
+                      </button>
                     </div>
                   </div>
                 </div>
               ))}
+            </div>
+          </div>
+          <div className="updateCard">
+            <div className="updateCardBox" style={{ display: `${display}` }}>
+              <div>
+                <label htmlFor="">Name </label>
+                <input
+                  type="text"
+                  placeholder="Enter Updated Name"
+                  value={data?.name}
+                  onChange={(e) => setData({ ...data, name: e.target.value })}
+                />
+              </div>
+              <div>
+                <label htmlFor="">Brand </label>
+                <input
+                  type="text"
+                  placeholder="Enter Updated Brand"
+                  value={data?.brand}
+                  onChange={(e) => setData({ ...data, brand: e.target.value })}
+                />
+              </div>
+              <div>
+                <label htmlFor="">Category </label>
+                <input
+                  type="text"
+                  placeholder="Enter Updated Category"
+                  value={data?.category}
+                  onChange={(e) =>
+                    setData({ ...data, category: e.target.value })
+                  }
+                />
+              </div>
+              <div>
+                <label htmlFor="">Sub Category </label>
+                <input
+                  type="text"
+                  placeholder="Enter Updated Sub Category"
+                  value={data?.subCategory}
+                  onChange={(e) =>
+                    setData({ ...data, subCategory: e.target.value })
+                  }
+                />
+              </div>
+              <div>
+                <label htmlFor="">Description </label>
+                <input
+                  type="text"
+                  placeholder="Enter Updated Description"
+                  value={data?.description}
+                  onChange={(e) =>
+                    setData({ ...data, description: e.target.value })
+                  }
+                />
+              </div>
+              <div>
+                <label htmlFor="">Stock </label>
+                <input
+                  type="text"
+                  placeholder="Enter Updated Stock"
+                  value={data?.Stock}
+                  onChange={(e) => setData({ ...data, Stock: e.target.value })}
+                />
+              </div>
+              <div>
+                <label htmlFor="">Price </label>
+                <input
+                  type="text"
+                  placeholder="Enter Updated Price"
+                  value={`₹ ${data?.price}`}
+                  onChange={(e) => setData({ ...data, price: e.target.value })}
+                />
+              </div>
+              <div>
+                <label htmlFor="">Deal of the Day </label>
+                <input
+                  type="text"
+                  placeholder="Enter Deal of the Day Status"
+                  value={data?.dealOfTheDay}
+                  onChange={(e) =>
+                    setData({ ...data, dealOfTheDay: e.target.value })
+                  }
+                />
+              </div>
+              <button>Update</button>
             </div>
           </div>
         </div>
